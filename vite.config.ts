076198e5fe46b5c253eef025209c269df5366ca4
timamikh <timamikh@gmail.com>
2025-10-29 13:15:04 +0300

    Fix base path for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/prospace_test_bot/',
+  base: './',
   plugins: [
     react(),
     VitePWA({
